a88d1f70439a31609542401a6ac12d1cb3ec712d
hardik <hardik.m@simformsolution.com>
2023-04-28 14:33:49 +0530

    refector code and remove console and use shorthand syntax

--- script.js
+++ script.js
@@ -7,66 +7,45 @@ const allCard = document.querySelector(".allcard");
 const greterIcon = document.querySelector(".graterslider");
 const lessIcon = document.querySelector(".lessslider");
 const maincard = document.querySelectorAll(".maincard");
-const closea=document.querySelector(".navbarul")
-const body = document.querySelector("body")
-const home =document.querySelector(".home")
-const scroll =document.querySelector(".scroll")
+const closea = document.querySelector(".navbarul");
+const body = document.querySelector("body");
+const home = document.querySelector(".home");
+const scroll = document.querySelector(".scroll");
 // function
 
 // relevan scroll
 
-
-
 let prevValue = 0;
 
-console.log(allCard);
-const next = function () {
-  maincard.forEach((x) => {
-    const g = x;
+const nextBUtton = function () {
+  maincard.forEach((element) => {
+    const changeCardPlace = element;
 
-    g.style.transform = `translateX(${prevValue - 268}px)`;
+    changeCardPlace.style.transform = `translateX(${prevValue - 268}px)`;
   });
 
   prevValue -= 268;
-  console.log(prevValue);
 
-  if (prevValue < 0) {
-    lessIcon.style.visibility = "visible";
-  }
+  lessIcon.style.visibility =prevValue < 0?  "visible":"hidden";
+  
 
-  else {
-    lessIcon.style.visibility = "hidden";
-  }
-
-  if (prevValue < (card.length - 6) * -268) {
-    greterIcon.style.visibility = "hidden";
-  } else {
-    greterIcon.style.visibility = "visible";
-  }
- 
+  greterIcon.style.visibility =
+    prevValue < (card.length - 6) * -268 ? "hidden" : "visible";
 };
 
 const previous = function () {
-  maincard.forEach((x) => {
-   
-    const g = x;
+  maincard.forEach((element) => {
+    const changeCardPlaceRight = element;
 
-    g.style.transform = `translateX(${prevValue + 268}px)`;
+    changeCardPlaceRight.style.transform = `translateX(${prevValue + 268}px)`;
   });
 
   prevValue += 268;
-  console.log(prevValue);
-  if (prevValue < 0) {
-    lessIcon.style.visibility = "visible";
-  } else {
-    lessIcon.style.visibility = "hidden";
-  }
-  if (prevValue < (card.length - 6) * -268) {
-    greterIcon.style.visibility = "hidden";
-  } else {
-    greterIcon.style.visibility = "visible";
-  }
 
+  lessIcon.style.visibility = prevValue < 0 ? "visible":"hidden"
+
+  greterIcon.style.visibility=prevValue < (card.length - 6) * -268? "hidden":"visible"
+  
 };
 let flag = false;
 let time;
@@ -80,10 +59,10 @@ const startScroll = function () {
       if (prevValue < (card.length - 6) * -268) {
         flag = true;
       } else {
-        next();
+        nextBUtton();
       }
     }
-  },1500);
+  }, 1500);
 };
 startScroll();
 
@@ -94,48 +73,50 @@ const stopScroll = function () {
 allCard.addEventListener("mouseover", stopScroll);
 allCard.addEventListener("mouseout", startScroll);
 
-if (prevValue === 0) {
+if (prevValue === 0){ 
   lessIcon.style.visibility = "hidden";
 }
-greterIcon.addEventListener("click", next);
+greterIcon.addEventListener("click", nextBUtton);
 
 lessIcon.addEventListener("click", previous);
 
 svgBtn.addEventListener("click", () => {
   allContent.style = "filter: blur(8px);";
   hideElement.classList.remove("hidden");
-  body.style.overflow='hidden'
-
+  body.style.overflow = "hidden";
 });
 allContent.addEventListener("click", () => {
-    body.style.overflow='visible'
+  body.style.overflow = "visible";
   allContent.style = "filter: blur(0px);";
   hideElement.classList.add("hidden");
 });
 closea.addEventListener("click", () => {
-    body.style.overflow='visible'
+  body.style.overflow = "visible";
   allContent.style = "filter: blur(0px);";
   hideElement.classList.add("hidden");
 });
 closeBtn.addEventListener("click", () => {
-    body.style.overflow='visible'
+  body.style.overflow = "visible";
   allContent.style = "filter: blur(0px);";
   hideElement.classList.add("hidden");
 });
 function isInViewport(el) {
   const rect = el.getBoundingClientRect();
   return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight ) &&
-      rect.right <= (window.innerWidth)
-
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= window.innerHeight &&
+    rect.right <= window.innerWidth
   );
 }
 
-document.addEventListener('scroll',()=>{
-   if(isInViewport(home)){
-    scroll.style.visibility="hidden"}
-    else{scroll.style.visibility="visible"}}, {
-  passive: true
-});
+document.addEventListener(
+  "scroll",
+  () => {
+    scroll.style.visibility = isInViewport(home)?"hidden":"visible";
+    
+  },
+  {
+    passive: true,
+  }
+);
